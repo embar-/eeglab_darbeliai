a4609d7743ae7e65596f4e75e83868c3f82ed017
embar- <embar@super.lt>
2015-01-27 15:10:23 +0200

    Neleisti atnaujinimo parinkčių, jei nėra atnaujinimas.m

--- UTF-8/konfig.m
+++ UTF-8/konfig.m
@@ -79,8 +79,12 @@ function konfig_OpeningFcn(hObject, eventdata, handles, varargin)
 
 function_dir=regexprep(mfilename('fullpath'),[ mfilename '$'], '' );
 Darbeliai_nuostatos_senos.lokale={ '' ; '' ; '' ; } ;
-Darbeliai_nuostatos_senos.tikrinti_versija=1;
-Darbeliai_nuostatos_senos.diegti_auto=0;
+if (exist('atnaujinimas','file') == 2) ;
+    Darbeliai_nuostatos_senos.tikrinti_versija=1;
+else    
+    Darbeliai_nuostatos_senos.tikrinti_versija=0;
+end;
+Darbeliai_nuostatos_senos.diegti_auto=0;  
 Darbeliai_nuostatos_senos.stabili_versija=0;
 Darbeliai_nuostatos_senos.savita_versija=0;
 Darbeliai_nuostatos_senos.url_atnaujinimui='';
@@ -292,6 +296,14 @@ function checkbox1_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of checkbox1
+
+if (exist('atnaujinimas','file') ~= 2) ;     
+    %set(handles.uipanel1,'Visible','off')
+    set(handles.checkbox1,'Enable','off');
+    set(handles.checkbox2,'Enable','off');
+    return; 
+end;
+
 if get(handles.checkbox1,'Value')
     set(handles.checkbox2,'Enable','on');
 else
