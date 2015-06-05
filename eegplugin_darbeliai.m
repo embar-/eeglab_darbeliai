efbfbf53b220fff143a11e919b962b5a2b40324b
embar- <embar@super.lt>
2015-06-05 23:01:54 +0300

    nepaisyti nenaudojamo Darbeliai.konfig_vers kitaip

--- eegplugin_darbeliai.m
+++ eegplugin_darbeliai.m
@@ -193,11 +193,11 @@ try
    Darbeliai_nuostatos.lokale=Darbeliai.nuostatos.lokale;
    Darbeliai_nuostatos.tikrinti_versija=Darbeliai.nuostatos.tikrinti_versija;
    Darbeliai_nuostatos.diegti_auto=Darbeliai.nuostatos.diegti_auto;
-   %Darbeliai_konfig_vers=Darbeliai.konfig_vers;
-   Darbeliai_nuostatos.stabili_versija=Darbeliai.nuostatos.stabili_versija;
-   Darbeliai_nuostatos.savita_versija=Darbeliai.nuostatos.savita_versija;
    Darbeliai_nuostatos.url_atnaujinimui=Darbeliai.nuostatos.url_atnaujinimui;
    Darbeliai_nuostatos.url_versijai=Darbeliai.nuostatos.url_versijai;
+   Darbeliai_nuostatos.stabili_versija=Darbeliai.nuostatos.stabili_versija;
+   Darbeliai_nuostatos.savita_versija=Darbeliai.nuostatos.savita_versija;
+   Darbeliai_konfig_vers=Darbeliai.konfig_vers;
 catch err;
    %disp(err.message);
 end;
