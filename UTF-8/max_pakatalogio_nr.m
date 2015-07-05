a34e8d0030e6221e2aa9b635739a7f4a7c6da31c
embar- <embar@super.lt>
2015-07-05 09:19:48 +0300

    Nenulūžti, jei skaičius nėra paprastas sveikasis (pvz., menamasis skaičius)

--- UTF-8/max_pakatalogio_nr.m
+++ UTF-8/max_pakatalogio_nr.m
@@ -5,5 +5,7 @@ d1=arrayfun(@(x) textscan(d0{x},'%[0123456789]'), 3:length(d0),'UniformOutput', 
 d2=[d1{:}];
 if isempty(d2); nr=0; return; end;
 d2=d2(find(~cellfun(@isempty,d2)));
-nr=max([d2{:}]);
+d3=[d2{:}];
+d4=[]; for i=1:length(d3); d4=[d4 str2num(d3{i})]; end;
+nr=max(d4);
 if isempty(nr); nr=0; end;
