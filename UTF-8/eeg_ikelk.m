60ed959e49ab4b043c7c6b44dc65b6452433cfd1
embar- <embar@super.lt>
2015-08-03 13:27:03 +0300

    eeg_ikelk.m: nepavykus įkelti, rodyti kitos įkėlimo programos pavadinimą

--- UTF-8/eeg_ikelk.m
+++ UTF-8/eeg_ikelk.m
@@ -10,17 +10,20 @@ function [EEG]=eeg_ikelk(Kelias, Rinkmena)
         % Pranesk_apie_klaida(lasterr, mfilename, Kelias_ir_rinkmena, 0);
         fprintf('\n%s\n%s...\n', Kelias_ir_rinkmena, lokaliz('ne EEGLAB rinkmena'));
         try % Importuoti per BIOSIG
+            fprintf('%s %s...\n', lokaliz('Trying again with'), 'BIOSIG');
             EEG=pop_biosig(Kelias_ir_rinkmena);
             EEG=eegh( ['pop_biosig(' Kelias_ir_rinkmena ')' ], EEG);
             if isempty(EEG.data); error(lokaliz('Empty dataset')); end;
         catch %; Pranesk_apie_klaida(lasterr, mfilename, Kelias_ir_rinkmena, 0);
             fprintf('%s...\n', lokaliz('BIOSIG negali nuskaityti'));
             try % Importuoti per FILEIO
+                fprintf('%s %s...\n', lokaliz('Trying again with'), 'FILEIO');
                 EEG=pop_fileio(Kelias_ir_rinkmena);
                 EEG=eegh( ['pop_fileio(' Kelias_ir_rinkmena ')' ], EEG);
             catch %; Pranesk_apie_klaida(lasterr, mfilename, Kelias_ir_rinkmena, 0);
                 fprintf('%s...\n', lokaliz('FILEIO negali nuskaityti'));
                 try % Įkelti tiesiogiai į MATLAB
+                    fprintf('%s %s...\n', lokaliz('Trying again with'), 'MATLAB');
                     load(Kelias_ir_rinkmena,'-mat');
                 catch %; Pranesk_apie_klaida(lasterr, mfilename, Kelias_ir_rinkmena, 0);
                     fprintf('%s...\n', lokaliz('MATLAB negali nuskaityti'));
