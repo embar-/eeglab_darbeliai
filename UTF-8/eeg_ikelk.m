2937bdaa97e577b70dcc7f7d1c9a09e6e6a53d3b
embar- <embar@super.lt>
2015-08-07 18:31:53 +0300

    EEG_ikelk.m: pakoreguoti per FILEIO įkeltus įvykių vardus

--- UTF-8/eeg_ikelk.m
+++ UTF-8/eeg_ikelk.m
@@ -1,5 +1,5 @@
-% eeg_ikelk - duomenų įkėlimas EEGLAB struktūros pavidalu kone iš bet kokios vienos EEG rinkmenos 
-% [EEG]=eeg_ikelk(kelias,rinkmena)
+% eeg_ikelk - duomenų įkėlimas EEGLAB struktūros pavidalu kone iš bet
+% kokios vienos EEG rinkmenos [EEG]=eeg_ikelk(kelias,rinkmena)
 
 function [EEG]=eeg_ikelk(Kelias, Rinkmena)
 
@@ -20,6 +20,11 @@ function [EEG]=eeg_ikelk(Kelias, Rinkmena)
                 fprintf('%s %s...\n', lokaliz('Trying again with'), 'FILEIO');
                 EEG=pop_fileio(Kelias_ir_rinkmena);
                 EEG=eegh( ['pop_fileio(' Kelias_ir_rinkmena ')' ], EEG);
+                % Sutvarkyti įvykių pavadinimus
+                try tipai=regexprep({EEG.event.type},'[\0]*$','');
+                    [EEG.event.type]=tipai{:};
+                catch
+                end;
             catch %; Pranesk_apie_klaida(lasterr, mfilename, Kelias_ir_rinkmena, 0);
                 fprintf('%s...\n', lokaliz('FILEIO negali nuskaityti'));
                 try % Įkelti tiesiogiai į MATLAB
