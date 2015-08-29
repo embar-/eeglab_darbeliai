6ea55e1204d5fdd829be99a133f823976c91a16c
embar- <embar@super.lt>
2015-08-29 07:40:33 +0300

    scroolplot2: mouseMoveCallback: ištaisyti –  parenkant „tėvus“, ignoruoti anotaciją po pele, kuri priklauso figūrai

--- external/scrollplot2.m
+++ external/scrollplot2.m
@@ -886,7 +886,7 @@ function mouseMoveCallback(varargin)
         if isempty(cAx);
             cAx=get(hFig,'currentAxes');
         end;
-        if isequal(overObcj,cAx) || is_family(overObcj,cAx) || strcmp(get(get(overObcj,'parent'),'tag'),scribeOverlay) ; % over main axis or over its child
+        if isequal(overObcj,cAx) || is_family(overObcj,cAx) || strcmp(get(get(overObcj,'parent'),'tag'), 'scribeOverlay') ; % over main axis or over its child
             if and(isempty(hMode),~strcmp(get(findall(cAx,'-property','Tag'),'Tag'),'scrollAx'));
                 fja1=get(cAx,'ButtonDownFcn');
                 branot=findall(get(findall(hFig,'Type','Annotationpane','Tag','scribeOverlay'),'Children'),'-not','Tag','Anot');
