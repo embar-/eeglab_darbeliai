9bb5f5fbdfc810334046a199679977bfec82ca96
embar- <embar@super.lt>
2016-06-09 12:49:13 +0300

    eeg_peržiūra: jei epochuoti įrašai nesuderinami - rodyti neperstumtas epochas

--- UTF-8/eeg_perziura.m
+++ UTF-8/eeg_perziura.m
@@ -101,14 +101,13 @@ if ~isempty(EEG2); disp('EEG2 perkeitimas...'); end;
 EEG2=perkeisk_eeg2(EEG2, reikia_ribozenkliu);
 
 sukeisk=0;
-if EEG1.trials == 1 && EEG2.trials > 1 && length(EEG2.times)/EEG2.srate/EEG2.trials < length(EEG1.times)/EEG1.srate ;
+if (EEG1.trials == 1) && (EEG2.trials > 1) && (length(EEG2.times)/EEG2.srate/EEG2.trials < length(EEG1.times)/EEG1.srate) ;
     sukeisk=1;
-elseif ~isempty(EEG2.times) && length(EEG1.times)/EEG1.srate > length(EEG2.times)/EEG2.srate && EEG1.trials <= EEG2.trials;
+elseif ~isempty(EEG2.times) && (length(EEG1.times)/EEG1.srate > length(EEG2.times)/EEG2.srate) && (EEG1.trials >= EEG2.trials);
     sukeisk=1;
-elseif EEG1.nbchan > EEG2.nbchan && length(EEG1.times)/EEG1.srate == length(EEG2.times)/EEG2.srate;
+elseif (EEG1.nbchan > EEG2.nbchan) && (length(EEG1.times)/EEG1.srate == length(EEG2.times)/EEG2.srate);
     sukeisk=1;
 end;
-
 if sukeisk; % EEG1 <-> EEG2
     EEG=EEG1; EEG1=EEG2; EEG2=EEG;
 end;
@@ -180,7 +179,8 @@ if isfield(EEG1.event, 'urevent') && isfield(EEG2.event, 'urevent');
                     EEG1.event(i).laikas_ms=EEG1.event(i).laikas_ms+m;
                 end;
             end;
-        else            
+        else
+            EEG1_kopija=EEG1;
             if EEG2.trials > 1;
                 [plotis1,pli]=max([trukme1,trukme2]);
                 if pli == 1; plotis1=plotis1+1/EEG1.srate; else plotis1=plotis1+1/EEG2.srate; end;
@@ -256,7 +256,11 @@ if isfield(EEG1.event, 'urevent') && isfield(EEG2.event, 'urevent');
                 EEG1.event(ismember(ivTipai,{'boundary'}))=[];
             end;
             if ~isempty(EEG1.times(diff(EEG1.times) < 0)) || ~isempty(EEG2.times(diff(EEG2.times) < 0));
-                warning('Įrašai nedera tarpusavyje! Matysite išdarkytą vaizdą!');
+                wrn=warning('off','backtrace');
+                warning('Epochuoti įrašai nedera tarpusavyje!');
+                warning(wrn.state, 'backtrace');
+                EEG1=EEG1_kopija; 
+                clear EEG1_kopija;
             end;
         end;
         EEG1.xmin=0.001*min(EEG1.times); if isempty(EEG1.xmin); EEG1.xmin=NaN; end;
